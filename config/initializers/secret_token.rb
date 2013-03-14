9db89f22fc9498da11834f5b99b96a1c1882b9d2
Brian Yingling <brian_yingling@yahoo.com>
2013-03-14 17:46:28 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homerkr::Application.config.secret_token = '6fd766dced8d2dc014cd908d7a112667e0a67f9beca4ce74048d7af036207718ef53e6732f6affbba79d6e6154ce9c108b9a64b53ef3f69d9f69115f2b1d2632'
